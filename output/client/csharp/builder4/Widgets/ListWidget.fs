c96bcd411405d37298ec0a7d4f0d9bfe8cab8521
Daniel X <nobody@nowhere.net>
2024-05-18 13:18:35 -0600

    some list widget methods (still woefully incomplete)

---
+++ output/client/csharp/builder4/Widgets/ListWidget.fs
@@ -0,0 +1,24 @@
+﻿module Widgets.ListWidget
+
+open System
+open BuilderNode
+open Org.Whatever.QtTesting
+
+type Signal =
+    | CurrentRowChanged of index: int option
+
+type SelectionMode =
+    | NotAllowed
+    | Single
+    | Extended
+    
+type Attr =
+    | Items of items: string list
+    | SelectionMode of mode: SelectionMode
+    
+let private attrKey = function
+    | Items _ -> 0
+    | SelectionMode _ -> 1
+
+let private diffAttrs =
+    genericDiffAttrs attrKey
