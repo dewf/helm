45a84dd9d641078d3ec0da307e33ed0717a4e4f2
Daniel X <nobody@nowhere.net>
2024-05-27 02:03:47 -0600

    flightbooker in progress, finally

---
+++ output/client/csharp/Demos/Tabs/FlightBooker/DateEdit.fs
@@ -0,0 +1,70 @@
+﻿module Tabs.FlightBooker.DateEdit
+
+open System
+open BuilderNode
+open SubReactor
+open Widgets
+
+type Signal =
+    | DateChanged of dt: DateTime
+    
+type Attr =
+    | Value of dt: DateTime
+    | Enabled of value: bool
+    
+let keyFunc = function
+    | Value _ -> 0
+    | Enabled _ -> 1
+    
+let diffAttrs =
+    genericDiffAttrs keyFunc
+    
+type State = {
+    Enabled: bool
+    Raw: string
+    MaybeParsed: DateTime option
+}
+
+type Msg =
+    | EditChanged of str: string
+
+let init () =
+    let dt =
+        DateTime.Now
+    let state = {
+        Enabled = true
+        MaybeParsed = Some dt
+        Raw = string dt
+    }
+    state, Cmd.None
+    
+let attrUpdate (state: State) (attr: Attr) =
+    match attr with
+    | Value dt ->
+        let nextState =
+            { state with
+                MaybeParsed = Some dt
+                Raw = string dt }
+        nextState
+    | Enabled value ->
+        { state with Enabled = value }
+    
+let update (state: State) (msg: Msg) =
+    // attempt to parse raw text into date
+    // update background color of edit if bad
+    state, Cmd.None
+
+let view (state: State) =
+    let edit =
+        LineEdit.Node(
+            Attrs = [ LineEdit.Value state.Raw; LineEdit.Enabled state.Enabled ], OnChanged = EditChanged)
+    edit :> IWidgetNode<Msg>
+    // let box =
+    //     BoxLayout.Node(
+    //         Attrs = [ BoxLayout.Direction BoxLayout.Vertical ],
+    //         Items = [ edit ])
+    // box :> ILayoutNode<Msg>
+    
+type Node<'outerMsg>() =
+    inherit WidgetReactorNode<'outerMsg, State, Msg, Attr, Signal>(init, attrUpdate, update, view, diffAttrs)
+
---
+++ output/client/csharp/Demos/Tabs/FlightBooker/FlightBooker.fs
@@ -0,0 +1,63 @@
+﻿module Tabs.FlightBooker.FlightBooker
+
+open System
+open BuilderNode
+open SubReactor
+open Widgets
+
+type Signal = unit
+type Attr = unit
+
+type Mode =
+    | OneWay
+    | RoundTrip
+
+type State = {
+    Mode: Mode
+    DepartDate: DateTime option
+    LeaveDate: DateTime option
+}
+
+type Msg =
+    | ComboChanged of maybeIndex: int option
+
+let init() =
+    { Mode = OneWay
+      DepartDate = Some DateTime.Now
+      LeaveDate = None }, Cmd.None
+    
+let update (state: State) (msg: Msg) =
+    match msg with
+    | ComboChanged maybeIndex ->
+        let nextMode = 
+            match maybeIndex with
+            | Some 0 -> OneWay
+            | Some 1 -> RoundTrip
+            | _ -> failwith "nope"
+        { state with Mode = nextMode }, Cmd.None
+
+let view (state: State) =
+    let combo =
+        let selectedIndex =
+            match state.Mode with
+            | OneWay -> 0
+            | RoundTrip -> 1
+        let items = [
+            "One Way"
+            "Round Trip"
+        ]
+        // PushButton.Node(Attrs = [ PushButton.Label "just testing" ])
+        ComboBox.Node(Attrs = [ ComboBox.Items items; ComboBox.SelectedIndex (Some selectedIndex) ], OnSelected = ComboChanged)
+    let edit1 =
+        DateEdit.Node()
+    let edit2 =
+        DateEdit.Node(Attrs = [ DateEdit.Enabled (state.Mode = RoundTrip) ])
+    let bookButton =
+        PushButton.Node(Attrs = [ PushButton.Label "Book Trip"; PushButton.Enabled false ])
+    BoxLayout.Node(
+        Attrs = [ BoxLayout.Direction BoxLayout.Vertical ],
+        Items = [ combo; edit1; edit2; bookButton ])
+    :> ILayoutNode<Msg>
+    
+type Node<'outerMsg>() =
+    inherit LayoutReactorNode<'outerMsg, State, Msg, unit, unit>(init, nullAttrUpdate, update, view, nullDiffAttrs)
