28ac5148243acfbc8ff94e9e61bb2b3e6dbbe267
Daniel X <nobody@nowhere.net>
2024-06-05 00:19:06 -0600

    WebpDestroyer test project

---
+++ output/client/csharp/WebpDestroyer/Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+
+open FSharpQt
+open FSharpQt.BuilderNode
+open Reactor
+open FSharpQt.Widgets
+open MainWindow
+
+
+type State = unit
+type Msg = unit
+
+let init() =
+    (), Cmd.None
+    
+let update (state: State) (msg: Msg) =
+    state, Cmd.None
+    
+let view (state: State) =
+    MainWindow(Attrs = [ Title "Woot"; Size (800, 600) ])
+    :> IBuilderNode<Msg>
+
+[<EntryPoint>]
+[<STAThread>]
+let main argv =
+    use app =
+        createApplication init update view
+    app.Run argv
