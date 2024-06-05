47949f91ef94ce6ac5bbae5c44b66ee41f6dc7af
Daniel X <nobody@nowhere.net>
2024-06-05 00:18:42 -0600

    new Qt version stuff, created quite a mess

--- output/server/_dllproject/build/cpp-debug-to-csharp-debug.bat
+++ output/server/_dllproject/build/cpp-debug-to-csharp-debug.bat
@@ -1,7 +1,14 @@
 copy /y .\cmake-build-debug\*.dll ..\..\..\client\csharp\AppRunner\bin\Debug\net8.0
+robocopy cmake-build-debug\plugins ..\..\..\client\csharp\AppRunner\bin\Debug\net8.0\plugins /e
+
 copy /y .\cmake-build-debug\*.dll ..\..\..\client\csharp\SevenGuisFsharp\bin\Debug\net8.0
+robocopy cmake-build-debug\plugins ..\..\..\client\csharp\SevenGuisFsharp\bin\Debug\net8.0\plugins /e
+
+copy /y .\cmake-build-debug\*.dll ..\..\..\client\csharp\WebpDestroyer\bin\Debug\net8.0
+robocopy cmake-build-debug\plugins ..\..\..\client\csharp\WebpDestroyer\bin\Debug\net8.0\plugins /e
+
 pause
 
 
 
-
+	
