b02468066ee7b4cd55b6400edb1ba3496a18aeaf
Daniel X <nobody@nowhere.net>
2024-06-06 11:54:46 -0600

    timer page reorganization (no real reason)

--- output/client/csharp/SevenGuisFsharp/Tabs/TimerPage.fs
+++ output/client/csharp/SevenGuisFsharp/Tabs/TimerPage.fs
@@ -44,38 +44,38 @@ let update (state: State) (msg: Msg) =
         nextState, Cmd.None
        
 let view (state: State) =
-    let progress =
-        let value =
-            (state.Accumulated * 1000) / state.Duration
-        let text =
-            sprintf "%.02fs" (float state.Accumulated / 1000.0)
-        ProgressBar(Attrs = [
-            Range (0, 1000) // using 1000 divisions ... setting duration directly here seemed to cause flickering
-            Value value
-            InnerText text
-        ])
-    let label =
-        let text =
-            sprintf "%.02fs" (float state.Duration / 1000.0)
-        Label(Attrs = [ Label.Text text ])
-    let slider =
-        Slider(Attrs = [
-            Orientation Horizontal
-            Slider.Range (100, 10_000)
-            TickPosition Below
-            TickInterval 1000
-            Slider.Value state.Duration
-            MinimumWidth 250
-        ], OnValueChanged = SetDuration)
-    let hbox =
-        BoxLayout(Attrs = [ Direction LeftToRight ],
-                  Items = [
-                      BoxItem.Create(label)
-                      BoxItem.Create(slider)
-                  ])
-    let button =
-        PushButton(Attrs = [ Text "Reset" ], OnClicked = Reset)
     let layout =
+        let progress =
+            let value =
+                (state.Accumulated * 1000) / state.Duration
+            let text =
+                sprintf "%.02fs" (float state.Accumulated / 1000.0)
+            ProgressBar(Attrs = [
+                Range (0, 1000) // using 1000 divisions ... setting duration directly here seemed to cause flickering
+                Value value
+                InnerText text
+            ])
+        let hbox =
+            let label =
+                let text =
+                    sprintf "%.02fs" (float state.Duration / 1000.0)
+                Label(Attrs = [ Label.Text text ])
+            let slider =
+                Slider(Attrs = [
+                    Orientation Horizontal
+                    Slider.Range (100, 10_000)
+                    TickPosition Below
+                    TickInterval 1000
+                    Slider.Value state.Duration
+                    MinimumWidth 250
+                ], OnValueChanged = SetDuration)
+            BoxLayout(Attrs = [ Direction LeftToRight ],
+                      Items = [
+                          BoxItem.Create(label)
+                          BoxItem.Create(slider)
+                      ])
+        let button =
+            PushButton(Attrs = [ Text "Reset" ], OnClicked = Reset)
         BoxLayout(Attrs = [ Direction TopToBottom ],
                   Items = [
                       BoxItem.Create(progress)
