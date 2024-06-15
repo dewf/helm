e872f166a96e697722ce51e8e698b7178d6ded77
Daniel X <nobody@nowhere.net>
2024-06-15 13:27:41 -0600

    tabwidget signal upgrade

--- output/client/csharp/FSharpQt/Widgets/TabWidget.fs
+++ output/client/csharp/FSharpQt/Widgets/TabWidget.fs
@@ -6,6 +6,9 @@ open Org.Whatever.QtTesting
 
 type Signal =
     | CurrentChanged of index: int
+    | TabBarClicked of index: int
+    | TabBarDoubleClicked of index: int
+    | TabCloseRequested of index: int
     
 type Attr =
     | NoneYet
@@ -16,48 +19,61 @@ let private keyFunc = function
 let private diffAttrs =
     genericDiffAttrs keyFunc
     
-type private Model<'msg>(dispatch: 'msg -> unit) =
+type private Model<'msg>(dispatch: 'msg -> unit) as this =
+    let mutable tabWidget = TabWidget.Create(this)
     let mutable signalMap: Signal -> 'msg option = (fun _ -> None)
-    let mutable tabWidget = TabWidget.Create()
+    let mutable currentMask = enum<TabWidget.SignalMask> 0
     
-    let addPages(pages: (string * Widget.Handle) list) =
-        for label, widget in pages do
-            tabWidget.AddTab(widget, label)
-    do
-        let signalDispatch (s: Signal) =
-            match signalMap s with
-            | Some msg ->
-                dispatch msg
-            | None ->
-                ()
-        tabWidget.OnCurrentChanged (fun index ->
-            signalDispatch (CurrentChanged index))
-        
-    member this.Refill(pages: (string * Widget.Handle) list) =
-        tabWidget.Clear()
-        addPages pages
+    let signalDispatch (s: Signal) =
+        match signalMap s with
+        | Some msg ->
+            dispatch msg
+        | None ->
+            ()
         
     member this.Widget with get() = tabWidget
-    member this.SignalMap with set(value) = signalMap <- value
+    member this.SignalMap with set value = signalMap <- value
     
+    member this.SignalMask with set value =
+        if value <> currentMask then
+            tabWidget.SetSignalMask(value)
+            currentMask <- value
+            
     member this.ApplyAttrs(attrs: Attr list) =
         attrs |> List.iter (function
             | NoneYet ->
                 ())
+    
+    member this.Refill(pages: (string * Widget.Handle) list) =
+        tabWidget.Clear()
+        for label, widget in pages do
+            tabWidget.AddTab(widget, label)
+            
+    interface TabWidget.SignalHandler with
+        member this.CurrentChanged index =
+            signalDispatch (CurrentChanged index)
+        member this.TabBarClicked index =
+            signalDispatch (TabBarClicked index)
+        member this.TabBarDoubleClicked index =
+            signalDispatch (TabBarDoubleClicked index)
+        member this.TabCloseRequested index =
+            signalDispatch (TabCloseRequested index)
         
     interface IDisposable with
         member this.Dispose() =
             tabWidget.Dispose()
 
-let private create (attrs: Attr list) (signalMap: Signal -> 'msg option) (dispatch: 'msg -> unit) =
+let private create (attrs: Attr list) (signalMap: Signal -> 'msg option) (dispatch: 'msg -> unit) (signalMask: TabWidget.SignalMask) =
     let model = new Model<'msg>(dispatch)
     model.ApplyAttrs attrs
     model.SignalMap <- signalMap
+    model.SignalMask <- signalMask
     model
 
-let private migrate (model: Model<'msg>) (attrs: Attr list) (signalMap: Signal -> 'msg option) =
+let private migrate (model: Model<'msg>) (attrs: Attr list) (signalMap: Signal -> 'msg option) (signalMask: TabWidget.SignalMask) =
     model.ApplyAttrs attrs
     model.SignalMap <- signalMap
+    model.SignalMask <- signalMask
     model
 
 let private dispose (model: Model<'msg>) =
@@ -70,14 +86,43 @@ type TabWidget<'msg>() =
     member val Pages: (string * IWidgetNode<'msg>) list = [] with get, set
     member val Attachments: (string * Attachment<'msg>) list = [] with get, set
     
-    let mutable onCurrentChanged: (int -> 'msg) option = None
-    member this.OnCurrentChanged with set value = onCurrentChanged <- Some value
+    let mutable signalMask = enum<TabWidget.SignalMask> 0
     
+    let mutable onCurrentChanged: (int -> 'msg) option = None
+    let mutable onTabBarClicked: (int -> 'msg) option = None
+    let mutable onTabBarDoubleClicked: (int -> 'msg) option = None
+    let mutable onTabCloseRequested: (int -> 'msg) option = None
+    
+    member this.OnCurrentChanged with set value =
+        onCurrentChanged <- Some value
+        signalMask <- signalMask ||| TabWidget.SignalMask.CurrentChanged
+        
+    member this.OnTabBarClicked with set value =
+        onTabBarClicked <- Some value
+        signalMask <- signalMask ||| TabWidget.SignalMask.TabBarClicked
+        
+    member this.OnTabBarDoubleClicked with set value =
+        onTabBarDoubleClicked <- Some value
+        signalMask <- signalMask ||| TabWidget.SignalMask.TabBarDoubleClicked
+        
+    member this.OnTabCloseRequested with set value =
+        onTabCloseRequested <- Some value
+        signalMask <- signalMask ||| TabWidget.SignalMask.TabCloseRequested
+
     let signalMap = function
         | CurrentChanged index ->
             onCurrentChanged
             |> Option.map (fun f -> f index)
-        
+        | TabBarClicked index ->
+            onTabBarClicked
+            |> Option.map (fun f -> f index)
+        | TabBarDoubleClicked index ->
+            onTabBarDoubleClicked
+            |> Option.map (fun f -> f index)
+        | TabCloseRequested index ->
+            onTabCloseRequested
+            |> Option.map (fun f -> f index)
+            
     member private this.MigrateContent(leftTabWidget: TabWidget<'msg>) =
         let leftContents =
             leftTabWidget.Pages
@@ -103,7 +148,7 @@ type TabWidget<'msg>() =
             |> List.mapi (fun i (_, node) -> IntKey i, node :> IBuilderNode<'msg>)
             
         override this.Create dispatch buildContext =
-            this.model <- create this.Attrs signalMap dispatch
+            this.model <- create this.Attrs signalMap dispatch signalMask
             
         override this.AttachDeps () =
             let pageLabelsAndHandles =
@@ -116,7 +161,7 @@ type TabWidget<'msg>() =
             let nextAttrs =
                 diffAttrs left'.Attrs this.Attrs
                 |> createdOrChanged
-            this.model <- migrate left'.model nextAttrs signalMap
+            this.model <- migrate left'.model nextAttrs signalMap signalMask
             this.MigrateContent(left')
             
         override this.Dispose() =
--- output/server/cpp/QtTestingImpl/TabWidget.cpp
+++ output/server/cpp/QtTestingImpl/TabWidget.cpp
@@ -2,12 +2,49 @@
 
 #include <QObject>
 #include <QTabWidget>
+#include <utility>
 
-#define THIS ((QTabWidget*)_this)
+#include "util/SignalStuff.h"
+
+#define THIS ((TabWidgetWithHandler*)_this)
 #define WIDGET(w) ((QWidget*)w)
 
 namespace TabWidget
 {
+    class TabWidgetWithHandler : public QTabWidget {
+        Q_OBJECT
+    private:
+        std::shared_ptr<SignalHandler> handler;
+        uint32_t lastMask = 0;
+        std::vector<SignalMapItem<SignalMask>> signalMap = {
+                { SignalMask::CurrentChanged, SIGNAL(currentChanged(int)), SLOT(onCurrentChanged(int)) },
+                { SignalMask::TabBarClicked, SIGNAL(tabBarClicked(int)), SLOT(onTabBarClicked(int)) },
+                { SignalMask::TabBarDoubleClicked, SIGNAL(tabBarDoubleClicked(int)), SLOT(onTabBarDoubleClicked(int)) },
+                { SignalMask::TabCloseRequested, SIGNAL(tabCloseRequested(int)), SLOT(onTabCloseRequested(int)) }
+        };
+    public:
+        explicit TabWidgetWithHandler(std::shared_ptr<SignalHandler> handler) : handler(std::move(handler)) {}
+        void setSignalMask(uint32_t newMask) {
+            if (newMask != lastMask) {
+                processChanges(lastMask, newMask, signalMap, this);
+                lastMask = newMask;
+            }
+        }
+    public slots:
+        void onCurrentChanged(int index) {
+            handler->currentChanged(index);
+        }
+        void onTabBarClicked(int index) {
+            handler->tabBarClicked(index);
+        }
+        void onTabBarDoubleClicked(int index) {
+            handler->tabBarDoubleClicked(index);
+        }
+        void onTabCloseRequested(int index) {
+            handler->tabCloseRequested(index);
+        }
+    };
+
     void Handle_addTab(HandleRef _this, Widget::HandleRef page, std::string label) {
         THIS->addTab(WIDGET(page), label.c_str());
     }
@@ -28,19 +65,17 @@ namespace TabWidget
         THIS->removeTab(index);
     }
 
-    void Handle_onCurrentChanged(HandleRef _this, std::function<IntDelegate> handler) {
-        QObject::connect(
-            THIS,
-            &QTabWidget::currentChanged,
-            THIS,
-            handler);
+    void Handle_setSignalMask(HandleRef _this, uint32_t mask) {
+        THIS->setSignalMask(mask);
     }
 
     void Handle_dispose(HandleRef _this) {
         delete THIS;
     }
 
-    HandleRef create() {
-        return (HandleRef)new QTabWidget();
+    HandleRef create(std::shared_ptr<SignalHandler> handler) {
+        return (HandleRef) new TabWidgetWithHandler(std::move(handler));
     }
 }
+
+#include "TabWidget.moc"
