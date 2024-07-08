33ebb37352c118975c21f7c505236d49b68d81e5
Daniel X <nobody@nowhere.net>
2024-07-08 05:13:13 -0600

    modelbinding: ViewContext seems like a very bad idea
    (or potentially even reading properties during update()! because they could conflict with something we're setting in the view ... sigh)

--- output/client/csharp/FSharpQt/ModelBindings.fs
+++ output/client/csharp/FSharpQt/ModelBindings.fs
@@ -18,12 +18,13 @@ type ModelBindingBase<'handle,'proxy> internal() =
     member this.UpdateContext =
         // always available, because tree built before first update
         maybeProxy.Value
-    member this.ViewContextDelayed =
-        // available only if tree has been built once!
-        // 'Delayed' because it will always lag 1 iteration behind whatever the current attributes are!!!
-        // if you need it to be current/synchronized, update your state during the update() with whatever proxy method you're interested in ...
-        // we do need a better mechanism to track signal-less Qt properties with synthetic signals
-        maybeProxy
+    // member this.ViewContextDelayed =
+    //     // available only if tree has been built once!
+    //     // 'Delayed' because it will always lag 1 iteration behind whatever the current attributes are!!!
+    //     // if you need it to be current/synchronized, update your state during the update() with whatever proxy method you're interested in ...
+    //     // we do need a better mechanism to track signal-less Qt properties with synthetic signals
+    //     // I would almost go as far as saying this should never be used, because it will lead to very confusing behavior
+    //     maybeProxy
     
 // should these be declared in the modules, as <module>.ModelBinding ?
 
